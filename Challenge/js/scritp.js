e48e159d80132551ec8696fc14adda1fba19ae71
Guilherme Mendes da Cunha <60897048+firgo12@users.noreply.github.com>
2023-09-25 16:57:50 -0300

    Adicionamento do charjs
    Charjs

--- Challenge/js/scritp.js
+++ Challenge/js/scritp.js
@@ -1,8 +1,9 @@
 let dropdown = document.getElementsByClassName("dropdown-btn");
+const ctx = document.getElementById('myChart_feedbackGeral');
 let i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
     let dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
@@ -12,3 +13,22 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+        scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
